419cac9861de1971ed1a21b060143284adf36903
nusrat <nusratadiba88@gmail.com>
2025-10-10 11:45:14 +0600

    feat: Add pages app

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+
 
 """
 
@@ -10,10 +9,25 @@ simply tack it on at the end, and it will display automatically on our future
 signup page.
 
 """
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+from .models import CustomUser
+
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("age",)
+        fields = UserCreationForm.Meta.fields + ("email", "age")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
+
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
